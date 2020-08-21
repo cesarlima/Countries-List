0053c751b5b36257e742f8051abe542507c5c1a7
Cesar <cesarcamposdelima@gmail.com>
2020-08-21 17:12:21 -0300

    Added remote repository

--- Domain/Repositories/ILocalCountryRepository.cs
+++ Domain/Repositories/ILocalCountryRepository.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Domain.Entities;
 
 namespace Domain.Repositories
 {
-    public interface ICountryRepository
+    public interface ILocalCountryRepository
     {
         IEnumerable<Country> GetByName(string name);
         IEnumerable<Country> GetByAlpha3Code(string alphaCode);
         IEnumerable<Country> GetByCurrencyCode(string currencyCode);
+        Task SaveAsync(IEnumerable<Country> countries);
     }
 }
---
+++ Domain/Repositories/IRemoteCountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IRemoteCountryRepository
+    {
+        Task<IEnumerable<Country>> GetByNameAsync(string name);
+        Task<IEnumerable<Country>> GetByAlpha3CodeAsync(string alphaCode);
+        Task<IEnumerable<Country>> GetByCurrencyCodeAsync(string currencyCode);
+    }
+}
