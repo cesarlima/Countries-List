021a368c49acb4131f72ab4a3338368721d69533
Cesar <cesarcamposdelima@gmail.com>
2020-08-21 16:19:06 -0300

    Added SearchCountryByCurrencyCodeUseCase

---
+++ Domain/UseCases/CountriesSearch/SearchCountryByCurrencyCodeUseCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Repositories;
+
+namespace Domain.UseCases.CountriesSearch
+{
+    public sealed class SearchCountryByCurrencyCodeUseCase : SearchCountryUseCase
+    {
+        private readonly ICountryRepository _countryRepository;
+
+        public SearchCountryByCurrencyCodeUseCase(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository ?? throw new ArgumentNullException(nameof(countryRepository));
+        }
+
+        public override IEnumerable<CountrySearchOutput> Execute(string input)
+        {
+            var countries = _countryRepository.GetByCurrencyCode(input);
+            var countriesOutput = CreateCountriesSearchOutputs(countries);
+
+            return countriesOutput;
+        }
+    }
+}
